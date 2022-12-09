8dac82744749f3f2a192a0181d9f8e9ec15a1ff8
Oliwjer <92600709+oliwjerr@users.noreply.github.com>
2022-12-10 02:42:16 +0300

    Add files via upload

---
+++ Çalışma3/1.py
@@ -0,0 +1,6 @@
+#liste=[]
+liste=["oliwjer",12,"5055052255523","bilmemne mah. 12. sok no:5"] #indeksler 0 dan başlar
+print(liste)
+print(liste[0])# o. indeksdeki eleman yazdırıldı
+liste.append("oliwjer") #listenin sonuna ekler
+print(liste)
---
+++ Çalışma3/6-for.py
@@ -0,0 +1,4 @@
+for i in range(1,10000):
+    print(i,"oliwjer")
+
+print("döngü bitti")
